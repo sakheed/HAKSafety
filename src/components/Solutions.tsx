9e7a7c265bef29b5e45145ff1a64dfce822a1dec
Kambria Rogalski <kambriar@udel.edu>
2023-04-30 01:50:23 -0400

    added shooting steps

--- src/components/Solutions.tsx
+++ src/components/Solutions.tsx
@@ -18,7 +18,35 @@ export function Solutions(): JSX.Element {
             setSoln("Secure your belongings, lock your dorm rooms");
         }
         if(event.target.value === "shooting"){
-            setSoln("Stay indoors, avoid being approached by strangers, walk in groups");
+            setSoln(<div>
+                <div>"Stay indoors, avoid being approached by strangers, walk in groups"</div>
+                <div>
+                    <b>Steps:</b><br/>
+                    <ol>
+                        <li>Evacuate the area and leave belongings behind. Call police when in a safe area (or press “Big Blue Button”).</li>
+                        <li>If evacuation is not possible, find a place out of view to hide and provide protection with nearby objects.</li>
+                        <li>Silence all electronics and remain quiet.</li>
+                        <li>Remain hidden until notified that it is safe to come out.
+                            <ol type='a'>
+                                <li>Always ensure it is the police, an active shooter may try to lure you out.</li>
+                            </ol>
+                        </li>
+                        <li>As a last resort, attempt to take the active shooter down. When the shooter is at close range and you cannot flee, your chance of survival is much greater if you try to incapacitate him/her.</li>
+                        <li>Call 911 for emergencies or call the non-emergency to report any incidents 302-831-2117</li>
+                    </ol>
+                    <b>Indoors:</b><br/>
+                    <ol>
+                        <li>If you are in a hallway, get into a room and secure the door, close blinds, and turn off lights.</li>
+                        <li>If possible, press the “Big Blue Button” to report the incident to the police. Immediately silence the device. </li>
+                        <li>As a last resort, attempt to take the active shooter down. When the shooter is at close range and you cannot flee, your chance of survival is much greater if you try to incapacitate him/her.</li>
+                    </ol>
+                    <b>Outdoors:</b><br/>
+                    <ol>
+                        <li>Find the nearest building and lock yourself inside. </li>
+                        <li>If you cannot find a building, continue to flee away from the sounds of shots and report the incident to the police when safe. </li>
+                    </ol>
+                </div>
+            </div>);
         }
 
     }
